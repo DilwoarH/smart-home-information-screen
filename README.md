f47dcadfea9dffc6ebf4f93c57643579e7797e7a
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-12-24 14:51:16 +0000

    Adds bus stop times

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# SmartHome information screen
+
+SmartHome is an information screen which displays useful information such as bus times, train line status and other information useful for modern day SmartHome.
+
+## API credits
+This application uses the [https://www.transportapi.com/](TransportAPI).
+
+## How to get Bus Stop ATOC
+1. On [https://www.openstreetmap.org/?layers=TD](OpenStreetMap) zoom right in on a bus stop you're interested in
+2. Click the bus stop node to reveal its tags on the left
+3. Copy the `naptan:AtcoCode` code.
+
+## Environment variables
+```
+REACT_APP_TRANSPORT_APP_ID=
+REACT_APP_TRANSPORT_API_KEY=
+REACT_APP_BUS_STOP_DATA=[{"id": "490000077E", "title": "My closest bus stop"},{"id": "490000077E", "title": "Towards Stratford station"}]
+```
 
 ## Available Scripts
 
@@ -26,19 +43,3 @@ The build is minified and the filenames include the hashes.<br>
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/components/bus-times/bus-times.js
+++ src/components/bus-times/bus-times.js
@@ -1,34 +1,135 @@
 import React, { Component } from 'react';
 import logo from '../../assets/tfl-logo.png';
 import './bus-times.css';
+import moment from 'moment';
 
 class BusTimes extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      app_id: process.env.REACT_APP_TRANSPORT_APP_ID,
+      app_key: process.env.REACT_APP_TRANSPORT_API_KEY,
+      isCheckingPeriod: this.isCheckingPeriod(),
+      config: this.getBusStopConfig(),
+      isLoaded: false,
+      error: false,
+      lastUpdated: this.getLastUpdatedTime(),
+      busStops: [],
+      busTimes: []
+    };
+  }
+
+  getBusStopConfig() {
+    return JSON.parse(process.env.REACT_APP_BUS_STOP_DATA);
+  }
+
+  isCheckingPeriod() {
+    return moment() < moment("9:00 PM", 'HH:mm A') && moment() > moment("6:00 AM", 'HH:mm A');
+  }
+
+  getBusTimes(busStopId) {
+    const { app_id, app_key, busTimes, isCheckingPeriod } = this.state;
+
+    if (busTimes[busStopId] || !isCheckingPeriod) return;
+
+    busTimes[busStopId] = { departures: [] };
+
+    fetch(`https://transportapi.com/v3/uk/bus/stop/${busStopId}/live.json?app_id=${app_id}&app_key=${app_key}&group=route&nextbuses=yes`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          var departures = result.departures;
+          Object.keys(departures).forEach(key => {
+            var routeTimes = {
+              route: key,
+              times: []
+            };
+      
+            for (var i = 0; i < departures[key].length; i++) {
+              var _time = {
+                direction: departures[key][i]["direction"],
+                date: departures[key][i]["expected_departure_date"],
+                time: departures[key][i]["expected_departure_time"]
+              };
+              routeTimes.times.push(_time);
+            }
+            
+            busTimes[busStopId].departures.push(routeTimes);
+      
+            this.setState({ busTimes: busTimes });
+          });      
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+
+  getLastUpdatedTime() {
+    return moment().format('h:mm A');
+  }
+
+  componentDidMount() {
+    const { config } = this.state;
+    config.map(stop => {
+      this.getBusTimes(stop.id);
+    })
+    this.setState({ isLoaded: true });
   }
 
   render() {
-    return (
-      <div className="BusTimesWrapper">
-        <img src={logo} className="BusTimesLogo" alt="tfl-logo" />
-        <div className="BusTimes">
-          <table className="BusTimesTable">
-            <thead>
-              <tr>
-                <th>Route number</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                
-              </tr>
-            </tbody>
-          </table>
+    const { isLoaded, error, isCheckingPeriod } = this.state;
+
+    if (!isCheckingPeriod) {
+      return <div>Bus times checking turned off from 9pm til 6am.</div>;
+    }
+    else if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Bus times loading...</div>;
+    } else {
+      const { lastUpdated, busTimes, config } = this.state;
+      return (
+        <div className="BusTimesWrapper">
+          <img src={logo} className="BusTimesLogo" alt="tfl-logo" />
+          <div className="BusTimes">
+            { config.map(stop => (
+              <div className="BusStopSection" key={stop.id}>
+                <h3>{stop.title}</h3>
+                <table>
+                  <tbody>
+                    { (busTimes[stop.id] && busTimes[stop.id].departures ? busTimes[stop.id].departures : [] ).map( 
+                      departure => (
+                        <tr key={`departure-${Math.random()}`}>
+                          <td><h2>{departure.route}</h2></td>
+                          <td>
+                            <ul>
+                              {departure.times.map(
+                                dateTime => (
+                                  <li key={`time-${Math.random()}`}>
+                                    <strong>{moment(`${dateTime.date} ${dateTime.time}`).fromNow()} </strong> 
+                                    - <small className="BusTimesDirection">{dateTime.direction}</small>
+                                  </li>
+                                )
+                              )}
+                            </ul>
+                          </td>
+                        </tr>
+                      )
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            ))}
+            <small>Last updated: {lastUpdated}</small>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
